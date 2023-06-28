01bb12dc42077b9122efbbf9739d34b05a0955f8
ruippeixe <66218687+ruippeixe@users.noreply.github.com>
2023-06-28 22:25:09 +0100

    get user bet input values

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+// bet option choice
+
 const options = document.querySelectorAll(".options div");
 
 options.forEach((option) => {
@@ -15,14 +17,48 @@ function chooseRandomOption() {
   const randomOption = Math.floor(Math.random() * 37);
 
   if (randomOption === 0) {
-    return 'circle';
+    return "circle";
   } else if (randomOption % 2 === 0) {
-    return 'square';
+    return "square";
   } else {
-    return 'triangle';
+    return "triangle";
   }
 }
 
 function compareOptions(playerOption, machineOption) {
   console.log(playerOption === machineOption ? "win!" : "try again");
 }
+
+// input values
+
+const userBetForm = document.querySelector("#userBetForm");
+
+userBetForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let inputValue = document.querySelector("#userBetAmount");
+  let userBetAmount = inputValue.value;
+
+  if (
+    !isNaN(userBetAmount) &&
+    userBetAmount !== "" &&
+    !userBetAmount.includes(" ")
+  ) {
+    console.log("Input value:", userBetAmount);
+  } else {
+    console.log("Invalid input! Please enter a numeric value.");
+  }
+
+  inputValue.value = "";
+});
+
+const oneHundred = document.querySelector("#oneHundred");
+const fiveHundred = document.querySelector("#fiveHundred");
+
+oneHundred.addEventListener("click", () => {
+  console.log("Clicked on oneHundred element");
+});
+
+fiveHundred.addEventListener("click", () => {
+  console.log("Clicked on fiveHundred element");
+});
